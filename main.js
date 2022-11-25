d3e7f8c4690f154a528b25c62a4d4290d1f137e7
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-25 11:56:22 +0100

    responsiveness

--- main.js
+++ main.js
@@ -19,3 +19,26 @@ faqs.forEach((faq) => {
     }
   });
 });
+
+//**toogle menu */
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+//! show nav menu
+const openNav = () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+};
+
+menuBtn.addEventListener("click", openNav);
+
+//! close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
